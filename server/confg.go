fb35ef885f30b6955d01408f1029fb78a24b4a27
Alvaro T <goodbytes23@gmail.com>
2024-10-22 09:26:55 +0200

    Refactor: Move checkEnvVarsConf and checkEnvVar to a new file.

---
+++ server/confg.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/alvarotor/user-go/server/model"
+)
+
+func checkEnvVarsConf(conf *model.Config) {
+	checkEnvVar("PROJECT_PORT")
+	checkEnvVar("POSTGRES_HOST")
+	checkEnvVar("POSTGRES_DB")
+	checkEnvVar("POSTGRES_USER")
+	checkEnvVar("POSTGRES_PASSWORD")
+	checkEnvVar("POSTGRES_PORT")
+	checkEnvVar("RandomStringValidation")
+	checkEnvVar("SizeRandomStringValidation")
+	checkEnvVar("Issuer")
+
+	PROJECT_PORT, err := strconv.Atoi(os.Getenv("PROJECT_PORT"))
+	if err != nil {
+		log.Fatalf(`Missing PROJECT_PORT env var`)
+	}
+	conf.PROJECT_PORT = PROJECT_PORT
+	conf.POSTGRES_HOST = os.Getenv("POSTGRES_HOST")
+	conf.POSTGRES_DB = os.Getenv("POSTGRES_DB")
+	conf.POSTGRES_USER = os.Getenv("POSTGRES_USER")
+	conf.POSTGRES_PASSWORD = os.Getenv("POSTGRES_PASSWORD")
+	conf.POSTGRES_PORT = os.Getenv("POSTGRES_PORT")
+	conf.RandomStringValidation = os.Getenv("RandomStringValidation")
+	SizeRandomStringValidation, err := strconv.Atoi(os.Getenv("SizeRandomStringValidation"))
+	if err != nil {
+		log.Fatalf(`Missing SizeRandomStringValidation env var`)
+	}
+	conf.SizeRandomStringValidation = SizeRandomStringValidation
+	conf.Issuer = os.Getenv("Issuer")
+}
+
+func checkEnvVar(envVar string) {
+	if len(os.Getenv(envVar)) == 0 {
+		log.Fatalf(`Missing %s env var`, envVar)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -6,7 +6,6 @@ import (
 	"log/slog"
 	"net"
 	"os"
-	"strconv"
 
 	"google.golang.org/grpc"
 
@@ -54,41 +53,5 @@ func main() {
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}
-}
-
-func checkEnvVarsConf(conf *model.Config) {
-	checkEnvVar("PROJECT_PORT")
-	checkEnvVar("POSTGRES_HOST")
-	checkEnvVar("POSTGRES_DB")
-	checkEnvVar("POSTGRES_USER")
-	checkEnvVar("POSTGRES_PASSWORD")
-	checkEnvVar("POSTGRES_PORT")
-	checkEnvVar("RandomStringValidation")
-	checkEnvVar("SizeRandomStringValidation")
-	checkEnvVar("Issuer")
-
-	PROJECT_PORT, err := strconv.Atoi(os.Getenv("PROJECT_PORT"))
-	if err != nil {
-		log.Fatalf(`Missing PROJECT_PORT env var`)
-	}
-	conf.PROJECT_PORT = PROJECT_PORT
-	conf.POSTGRES_HOST = os.Getenv("POSTGRES_HOST")
-	conf.POSTGRES_DB = os.Getenv("POSTGRES_DB")
-	conf.POSTGRES_USER = os.Getenv("POSTGRES_USER")
-	conf.POSTGRES_PASSWORD = os.Getenv("POSTGRES_PASSWORD")
-	conf.POSTGRES_PORT = os.Getenv("POSTGRES_PORT")
-	conf.RandomStringValidation = os.Getenv("RandomStringValidation")
-	SizeRandomStringValidation, err := strconv.Atoi(os.Getenv("SizeRandomStringValidation"))
-	if err != nil {
-		log.Fatalf(`Missing SizeRandomStringValidation env var`)
-	}
-	conf.SizeRandomStringValidation = SizeRandomStringValidation
-	conf.Issuer = os.Getenv("Issuer")
-}
-
-func checkEnvVar(envVar string) {
-	if len(os.Getenv(envVar)) == 0 {
-		log.Fatalf(`Missing %s env var`, envVar)
 	}
 }
