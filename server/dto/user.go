3f20d2ca69b4a3b35531b389aec87977d57459e5
Alvaro T <goodbytes23@gmail.com>
2024-10-28 21:51:53 +0100

    Remove unused UserResponse and UserRequest structs

--- server/dto/user.go
+++ server/dto/user.go
@@ -16,21 +16,3 @@ type UserLogin struct {
 	Email string `json:"email" validate:"email,required"`
 	Time  uint   `json:"time" validate:"required,gt=0 lt=60"`
 }
-
-type UserResponse struct {
-	Name          string `json:"name"`
-	Email         string `json:"email"`
-	Validated     bool   `json:"validated"`
-	ProfilePic    string `json:"profilePic"`
-	Gender        uint   `json:"gender"`
-	Age           uint   `json:"age"`
-	CountryOrigin string `json:"countryOrigin"`
-}
-
-type UserRequest struct {
-	Name          string `json:"name" validate:"required"`
-	Validated     bool   `json:"validated"`
-	Gender        uint   `json:"gender"`
-	Age           uint   `json:"age"`
-	CountryOrigin string `json:"countryOrigin"`
-}
